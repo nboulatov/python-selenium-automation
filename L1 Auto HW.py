61fed9656e8e1e3eef0d1d92c5e57b6b870514da
nboulatov <nboulatov@gmail.com>
2024-01-25 06:57:32 -0800

    Update L1 Auto HW

--- L1 Auto HW.py
+++ L1 Auto HW.py
@@ -11,15 +11,15 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&")
 driver.maximize_window()
 time.sleep(10)
-Amazon_logo = driver.find_element(By.XPATH, "//i[@aria-label='Amazon']").text
-Email_field = driver.find_element(By.XPATH, "//input[@id='ap_email']").text
-Continue_button = driver.find_element(By.XPATH, "//input[@id='continue']").text
-Conditions_of_use = driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(text(),'Conditions of Use')]").text
-Privacy_Notice = driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(text(),'Privacy Notice')]").text
-Need_help = driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']").text
-Forgot_your_password = driver.find_element(By.XPATH, "(//a[normalize-space()='Forgot your password?'])[1]").text
-# Where is this text (Other_issues_with_Sign_In)? I do not see it on the website. As you can tell from the above/below code, I have the understanding of xpath creation process. I do not want to loose points for something I do not see on the page. Please consider this before grading. Thank you.
-Create_your_Amazon_account = driver.find_element(By.XPATH, "//a[@id='createAccountSubmit']").text
+amazon_logo = driver.find_element(By.XPATH, "//i[@aria-label='Amazon']").text
+email_field = driver.find_element(By.XPATH, "//input[@id='ap_email']").text
+continue_button = driver.find_element(By.XPATH, "//input[@id='continue']").text
+conditions_of_use = driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(text(),'Conditions of Use')]").text
+privacy_notice = driver.find_element(By.XPATH, "//div[@id='legalTextRow']//a[contains(text(),'Privacy Notice')]").text
+need_help = driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']").text
+forgot_your_password = driver.find_element(By.XPATH, "(//a[normalize-space()='Forgot your password?'])[1]").text
+# Where is this text (other_issues_with_sign_In)? I do not see it on the website. As you can tell from the above/below code, I have the understanding of xpath creation process. I do not want to loose points for something I do not see on the page. Please consider this before grading. Thank you.
+create_your_cmazon_account = driver.find_element(By.XPATH, "//a[@id='createAccountSubmit']").text
 
 # 2. Create a test case for the SignIn page using python & selenium script.
 driver.get("https://www.target.com/") # 1. Open https://www.target.com/
@@ -31,13 +31,13 @@ time.sleep(3)
 text_to_verify = 'Target account'
 sign_in_text = driver.find_element(By.XPATH, "//span[normalize-space()='Sign into your Target account']").text # “Sign into your Target account” text is shown
 driver.find_element(By.XPATH, "//button[@id='login']") # SignIn button is shown (you can just use driver.find_elemennt() to check for element’s presence, no need to assert here)
-assert text_to_verify in sign_in_text
+assert text_to_verify in sign_in_text f'Expected {text_to_verify} not in {sign_in_text}'
 
 # [Optional] Build a test case yourself from scratch to search for a product on Target (same as shown in the class), make sure it works and you remember selenium commands.
 driver.get("https://www.target.com/") # 1. Open https://www.target.com/
 driver.maximize_window()
 product = 'coffee'
-driver.find_element(By.XPATH, "//input[@id='search']").send_keys(product )
+driver.find_element(By.ID, "search").send_keys(product)
 driver.find_element(By.XPATH, "//button[normalize-space()='search']").click()
 time.sleep(3)
 actual_text = driver.find_element(By.XPATH, "//div[@data-test='resultsHeading']/span[contains(text(), 'coffee')]").text
